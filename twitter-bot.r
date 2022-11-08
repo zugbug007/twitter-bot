4f29ff638288ddc71860d33df67bf2a6c27bed77
Alan Millington <zugbug007@users.noreply.github.com>
2022-11-08 23:27:32 +0000

    Update twitter-bot.r

--- twitter-bot.r
+++ twitter-bot.r
@@ -1,8 +1,15 @@
 library(rtweet)
 
+# Create Twitter token
+nttwitterbot_token <- rtweet::rtweet_bot(
+  api_key       = Sys.getenv("BOT_TWITTERAPIKEY"),
+  api_secret    = Sys.getenv("BOT_TWITTERAPISECRETKEY"),
+  access_token  = Sys.getenv("BOT_TWITTERACCESSTOKEN"),
+  access_secret = Sys.getenv("BOT_TWITTERACCESSTOKENSECRET")
+)
+
 # Build the status message (text and URL)
 place_details <- paste0("test")
-
 
 # Post the tweet to Twitter
 rtweet::post_tweet(
